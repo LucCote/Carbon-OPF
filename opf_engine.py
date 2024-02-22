00a3809ffd38f2a294452837b96e355025bdeb53
LucCote <hockey.luc66@gmail.com>
2024-02-22 01:07:17 -0500

    added initial carbon model

--- opf_engine.py
+++ opf_engine.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import gurobipy as gp
-from gurobipy import GRB
+from gurobipy import GRB, max_
 from gurobipy import quicksum
 from gurobipy import LinExpr
 
@@ -13,6 +13,10 @@ nodes = 3
 P_load = np.array([-1,0,-3])
 
 branch_limits = np.array([[0,10,0],[10,0,10],[0,10,0]])
+
+generator_carbon = np.array([1,0,2])
+
+carbon_upper_bounds = np.array([1.25,2,2])
 
 branch_reactance = np.array([[1,1,1],[1,1,1],[1,1,1]])
 B = 1/branch_reactance
@@ -32,6 +36,8 @@ def create_opf_model(nodes,gen_costs,branch_limits,B,gen_upper_bounds):
     P_gen = m.addVars(nodes,lb=np.zeros(nodes),ub = gen_upper_bounds, vtype= GRB.CONTINUOUS)
     voltage_angle = m.addVars(nodes,lb=-1000*np.ones(nodes),vtype= GRB.CONTINUOUS)
     Flow = m.addVars(nodes,nodes,lb=-1000*np.ones((nodes,nodes)), ub = branch_limits, vtype = GRB.CONTINUOUS)
+    P_in = m.addMVar((nodes,nodes), lb=np.zeros((nodes,nodes)),vtype= GRB.CONTINUOUS)
+    P_b = m.addMVar((nodes,nodes), lb=np.zeros((nodes,nodes)),vtype= GRB.CONTINUOUS)
 
     # add constraints
 
@@ -53,6 +59,18 @@ def create_opf_model(nodes,gen_costs,branch_limits,B,gen_upper_bounds):
                  for j in range(nodes))
 
     m.addConstr(voltage_angle[0] == 0)
+    for j in range(nodes):
+        for i in range(nodes):
+            m.addGenConstrMax(P_b[j,i],[Flow[j,i],0])
+    
+    m.addConstrs((quicksum(P_b[j,i] for j in range(nodes)) 
+                == P_in[i,i] for i in range(nodes)))
+
+    m.addConstrs(0 == P_in[i,j] for i in range(nodes) for j in range(i+1,nodes))
+    m.addConstrs(0 == P_in[i,j] for i in range(nodes) for j in range(0,i))
+
+    # m.addConstrs(np.dot((P_in-P_b,carbon_upper_bounds)) >= generator_carbon)
+    m.addConstr((P_in-P_b) @ carbon_upper_bounds >= generator_carbon)
 
     # add objective
 
