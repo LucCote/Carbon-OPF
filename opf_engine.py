c7172eee2a48fcd7ec21c69de4f6b51eaf4a7f30
alexddot <alex.derenchuk@gmail.com>
2024-02-22 19:34:17 -0500

    fixed bug; loads now negative and OPF solves with case3

--- opf_engine.py
+++ opf_engine.py
@@ -21,7 +21,7 @@ def read_data_from_files(gen_file,bus_file,branch_file,gen_cost_file,case):
     gencost_data_case = gencost_data.loc[gencost_data['case'] == case]
 
     nodes = bus_data_case['bus'].max()
-    P_load = bus_data_case.Pd.tolist()
+    P_load = (-1 * bus_data_case.Pd).tolist()
 
     gen_bus = gen_data_case.bus.tolist()
     gen_upper_bounds = gen_data_case.Pmax.tolist()
@@ -33,11 +33,16 @@ def read_data_from_files(gen_file,bus_file,branch_file,gen_cost_file,case):
     for i,row in branch_data_case.iterrows():
         i = int(row.fbus)-1
         j = int(row.tbus)-1
+
         branch_limits[i,j] = row.rateA
         B[i,j] = 1 / row.x
 
+        branch_limits[j,i] = row.rateA
+        B[j,i] = 1 / row.x
+
         if branch_limits[i,j] == 0:
                 B[i,j] = 0
+                B[j,i] = 0
 
     return (nodes,
             P_load,
@@ -56,8 +61,8 @@ def create_opf_model(nodes,gen_costs,branch_limits,B,gen_upper_bounds,generator_
     P_gen = m.addVars(nodes,lb=np.zeros(nodes),ub = gen_upper_bounds, vtype= GRB.CONTINUOUS)
     voltage_angle = m.addVars(nodes,lb=-1000*np.ones(nodes),vtype= GRB.CONTINUOUS)
     Flow = m.addVars(nodes,nodes,lb=-1000*np.ones((nodes,nodes)), ub = branch_limits, vtype = GRB.CONTINUOUS)
-    P_in = m.addMVar((nodes,nodes), lb=np.zeros((nodes,nodes)),vtype= GRB.CONTINUOUS)
-    P_b = m.addMVar((nodes,nodes), lb=np.zeros((nodes,nodes)),vtype= GRB.CONTINUOUS)
+    #P_in = m.addMVar((nodes,nodes), lb=np.zeros((nodes,nodes)),vtype= GRB.CONTINUOUS)
+    #P_b = m.addMVar((nodes,nodes), lb=np.zeros((nodes,nodes)),vtype= GRB.CONTINUOUS)
 
     # add constraints
 
@@ -79,18 +84,19 @@ def create_opf_model(nodes,gen_costs,branch_limits,B,gen_upper_bounds,generator_
                  for j in range(nodes))
 
     m.addConstr(voltage_angle[0] == 0)
-    for j in range(nodes):
-        for i in range(nodes):
-            m.addGenConstrMax(P_b[j,i],[Flow[j,i],0])
+
+    # for j in range(nodes):
+    #     for i in range(nodes):
+    #         m.addGenConstrMax(P_b[j,i],[Flow[j,i],0])
     
-    m.addConstrs((quicksum(P_b[j,i] for j in range(nodes)) 
-                == P_in[i,i] for i in range(nodes)))
+    # m.addConstrs((quicksum(P_b[j,i] for j in range(nodes)) 
+    #             == P_in[i,i] for i in range(nodes)))
 
-    m.addConstrs(0 == P_in[i,j] for i in range(nodes) for j in range(i+1,nodes))
-    m.addConstrs(0 == P_in[i,j] for i in range(nodes) for j in range(0,i))
+    # m.addConstrs(0 == P_in[i,j] for i in range(nodes) for j in range(i+1,nodes))
+    # m.addConstrs(0 == P_in[i,j] for i in range(nodes) for j in range(0,i))
 
-    # m.addConstrs(np.dot((P_in-P_b,carbon_upper_bounds)) >= generator_carbon)
-    m.addConstr((P_in-P_b) @ carbon_upper_bounds >= generator_carbon)
+    # # m.addConstrs(np.dot((P_in-P_b,carbon_upper_bounds)) >= generator_carbon)
+    # m.addConstr((P_in-P_b) @ carbon_upper_bounds >= generator_carbon)
 
     # add objective
 
@@ -115,6 +121,26 @@ B) = read_data_from_files(gen_file,bus_file,branch_file,gen_cost_file,case)
 
 generator_carbon = np.array([1,0,2])
 carbon_upper_bounds = np.array([1.25,2,2])
+
+
+# nodes = 3
+# P_load = np.array([-1,0,-3])
+
+# branch_limits = np.array([[0,10,0],[10,0,10],[0,10,0]])
+
+# generator_carbon = np.array([1,0,2])
+
+# carbon_upper_bounds = np.array([1.25,2,2])
+
+# branch_reactance = np.array([[1,1,1],[1,1,1],[1,1,1]])
+# B = 1/branch_reactance
+# for i in range(len(branch_limits)):
+#     for j in range(len(branch_limits[i])):
+#         if branch_limits[i,j] == 0:
+#             B[i,j] = 0
+
+# gen_upper_bounds = np.array([2,0,2]) # generator capacity
+# gen_costs = np.array([10,15,12]) # gen cost per ouput
 
 m = create_opf_model(nodes,
                      gen_costs,
