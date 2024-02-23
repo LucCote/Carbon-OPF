c9c0464af9cec19d91f078353873697756b0454d
LucCote <hockey.luc66@gmail.com>
2024-02-23 10:51:08 -0500

    fixed carbon constraints

--- opf_engine.py
+++ opf_engine.py
@@ -48,16 +48,16 @@ def read_data_from_files(gen_file,bus_file,branch_file,gen_cost_file,case):
             B)
 
 
-def create_opf_model(nodes,gen_costs,branch_limits,B,gen_upper_bounds,generator_carbon,carbon_upper_bounds):
+def create_opf_model(nodes,gen_costs,branch_limits,B,gen_upper_bounds,r_g,w_bar):
     
     m = gp.Model()
 
     # add variables
     P_gen = m.addVars(nodes,lb=np.zeros(nodes),ub = gen_upper_bounds, vtype= GRB.CONTINUOUS)
     voltage_angle = m.addVars(nodes,lb=-1000*np.ones(nodes),vtype= GRB.CONTINUOUS)
-    Flow = m.addVars(nodes,nodes,lb=-1000*np.ones((nodes,nodes)), ub = branch_limits, vtype = GRB.CONTINUOUS)
-    P_in = m.addMVar((nodes,nodes), lb=np.zeros((nodes,nodes)),vtype= GRB.CONTINUOUS)
-    P_b = m.addMVar((nodes,nodes), lb=np.zeros((nodes,nodes)),vtype= GRB.CONTINUOUS)
+    Flow = m.addVars(nodes,nodes,lb=-1000*np.ones((nodes,nodes)), ub = branch_limits, vtype = GRB.CONTINUOUS, name=["F"+str(i)+str(j) for i in range(nodes) for j in range(nodes)])
+    P_n = m.addMVar((nodes,nodes), lb=np.zeros((nodes,nodes)),vtype= GRB.CONTINUOUS,name="Pn")
+    P_b = m.addMVar((nodes,nodes), lb=np.zeros((nodes,nodes)),vtype= GRB.CONTINUOUS,name="Pb")
 
     # add constraints
 
@@ -79,18 +79,19 @@ def create_opf_model(nodes,gen_costs,branch_limits,B,gen_upper_bounds,generator_
                  for j in range(nodes))
 
     m.addConstr(voltage_angle[0] == 0)
+    
     for j in range(nodes):
         for i in range(nodes):
-            m.addGenConstrMax(P_b[j,i],[Flow[j,i],0])
+            m.addGenConstrMax(P_b[j,i],[Flow[i,j],0])
     
-    m.addConstrs((quicksum(P_b[j,i] for j in range(nodes)) 
-                == P_in[i,i] for i in range(nodes)))
+    m.addConstrs((quicksum(P_b[j,i] for j in range(nodes))+P_gen[i] 
+                == P_n[i,i] for i in range(nodes)))
 
-    m.addConstrs(0 == P_in[i,j] for i in range(nodes) for j in range(i+1,nodes))
-    m.addConstrs(0 == P_in[i,j] for i in range(nodes) for j in range(0,i))
+    m.addConstrs(0 == P_n[i,j] for i in range(nodes) for j in range(i+1,nodes))
+    m.addConstrs(0 == P_n[i,j] for i in range(nodes) for j in range(0,i))
 
     # m.addConstrs(np.dot((P_in-P_b,carbon_upper_bounds)) >= generator_carbon)
-    m.addConstr((P_in-P_b) @ carbon_upper_bounds >= generator_carbon)
+    m.addConstr(r_g <= (P_n-P_b) @ w_bar)
 
     # add objective
 
@@ -113,6 +114,25 @@ gen_costs,
 branch_limits,
 B) = read_data_from_files(gen_file,bus_file,branch_file,gen_cost_file,case)
 
+nodes = 3
+P_load = np.array([-1,0,-3])
+
+branch_limits = np.array([[0,10,0],[10,0,10],[0,10,0]])
+
+generator_carbon = np.array([1,0,2])
+
+carbon_upper_bounds = np.array([1.25,2,2])
+
+branch_reactance = np.array([[1,1,1],[1,1,1],[1,1,1]])
+B = 1/branch_reactance
+for i in range(len(branch_limits)):
+    for j in range(len(branch_limits[i])):
+        if branch_limits[i,j] == 0:
+            B[i,j] = 0
+
+gen_upper_bounds = np.array([2,0,2]) # generator capacity
+gen_costs = np.array([10,15,12]) # gen cost per ouput
+
 generator_carbon = np.array([1,0,2])
 carbon_upper_bounds = np.array([1.25,2,2])
 
@@ -125,6 +145,8 @@ m = create_opf_model(nodes,
                      carbon_upper_bounds)
 
 m.optimize()
+# m.computeIIS()
+# m.write("test.ilp")
 print(m.getVars())
 
 
